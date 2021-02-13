2dc055d9786bd4f30445809d6d2d9becb2b647f3
salman <salmanpigi@gmail.com>
2021-02-13 21:49:35 +0700

    fix dependencies

--- src/main/java/com/enigma/api/inventory/Application.java
+++ src/main/java/com/enigma/api/inventory/Application.java
@@ -1,11 +1,8 @@
 package com.enigma.api.inventory;
 
-import org.apache.tika.Tika;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
-import java.net.URL;
 
 @SpringBootApplication
 public class Application {
